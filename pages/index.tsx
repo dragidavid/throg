854c7b87acb30dfbbde71351c9e7c37ea24e5703
David Dragovacz <david.dragovacz@deliveroo.co.uk>
2022-06-08 20:53:27 +0100

    on brand

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-9xl font-extrabold">hello</h1>
+      <h1 className="text-9xl font-extrabold">🐸</h1>
     </div>
   );
 };
